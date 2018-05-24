5905695beed55df55529f136f9720197fe0cfe49
Aleksey Pozdeev <Aleksey.Pozdeev@tatar.ru>
2018-05-24 16:15:55 +0300

    Добавлены комментарии

--- scanner.php
+++ scanner.php
@@ -5,23 +5,47 @@ use Model\Disk\Disk;
 use Service\DI\DI;
 use Service\Scanner\ScannerException;
 
+header("Content-type: text/html; charset=utf-8");
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
+/* Функцтя возврата сосотояния сканеру */
+function stateReturn (bool $success, string $message) {
+    /* Пишем в лог либо ошибку либо успешно добавленную запись */
+    $success ? DI::Log()->info($message) : DI::Log()->error($message);
+
+    /* выводим сообщение сканеру */
+    exit(
+        json_encode([
+            'success' => $success,
+            'message' => $message
+        ])
+    );
+}
+
+/* получаем от сканера данные */
 $data = DI::Request()->get('dataScaner');
-if($data) {
-    try {
-        $fields = DI::Scanner()->dataReception($data);
 
-        if($fields) {
-            $model = $fields['isbn'] ? new Book() : new Disk();
-            $model->assign($fields)->save();
+/* если данных нет, то говорим об этом сканеру*/
+if(!$data) {
+    stateReturn(false, 'No input data');
+}
 
-            DI::Log()->info('Added successfully "' . $model->title . '" ID: ' . $model->id);
-        } else {
-            DI::Log()->error(DI::Scanner()->getErrors());
-        }
-    } catch (Exception $e) {
-        DI::Log()->error($e->getMessage());
-        throw new ScannerException("Unable to write data");
+/* пытаемся распознать данные и записать их в базу */
+try {
+    /* если данные не удалось распарсить*/
+    if(!$fields = DI::Scanner()->dataReception($data)) {
+        stateReturn(false, DI::Scanner()->getErrors());
     }
+
+    /* если в данных есть поле ISBN, значит создаем объект книги иначе диска */
+    $model = $fields['isbn'] ? new Book() : new Disk();
+    $model->assign($fields)->save();
+
+    /* сообщаем сканеру об успешной операции */
+    stateReturn(true, 'Added successfully "' . $model->title . '" ID: ' . $model->id);
+} catch (Exception $e) {
+    /* если что-то пошло не так */
+    stateReturn(false, $e->getMessage());
+    throw new ScannerException("Unable to write data");
 }
